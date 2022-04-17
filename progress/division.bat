9123014dd843d00cbf6414eff90a7c745f053ef0
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-04-17 13:23:07 -0400

    Create division.bat

---
+++ progress/division.bat
@@ -0,0 +1,16 @@
+@echo off
+:division
+cls
+title DIVISION
+echo.
+set /p nro1= Coloque la primera cifra: 
+echo.
+set /p nro2= Coloque la segunda cifra: 
+cls
+
+set /a resta= "%nro1%" - "%nro2%"
+
+echo.
+echo El resultado de la suma es %division%
+pause >nul
+start inicio.bat
