be3a623284366b3075e1b87f211181d1a187538a
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-04-17 13:49:50 -0400

    Create Calculadora-.bat

---
+++ Calculadora-.bat
@@ -0,0 +1,20 @@
+@echo off
+:inicio
+title Calculadora-
+echo.
+set /p name= Hola Amigo/a, como te llamas? Yo: 
+echo.
+echo Hola %name%, soy una Calculadora- que ayuda a operar 
+echo tus problemas matematicos
+echo.
+echo 1. Suma
+echo 2. Resta
+echo 3. Multiplicacion
+echo 4. Division
+echo.
+set /p progress_inicio= 
+
+if "%progress_inicio%"=="" goto progress_inicio
+:progress_inicio
+start comunitation/progress_inicio.bat
+exit
