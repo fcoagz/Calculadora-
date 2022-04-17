a2974c2aa050cc221fe34e8d6631129432eb6111
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-04-17 13:18:47 -0400

    Create suma.bat

---
+++ progress/suma.bat
@@ -0,0 +1,16 @@
+@echo off
+:suma
+cls
+title SUMA
+echo.
+set /p nro1= Coloque la primera cifra: 
+echo.
+set /p nro2= Coloque la segunda cifra: 
+cls
+
+set /a suma= "%nro1%" + "%nro2%"
+
+echo.
+echo El resultado de la suma es %suma%
+pause >nul
+start inicio.bat
