3247bdb955321c6ecf358c80c759e8ff630e2673
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-04-17 15:52:19 -0400

    Update Calculadora-.bat

--- Calculadora-.bat
+++ Calculadora-.bat
@@ -13,22 +13,3 @@ echo 3. Multiplicacion
 echo 4. Division
 echo.
 set /p progress_inicio= 
-
-if "%progress_inicio%"=="" goto progress_inicio
-:progress_inicio
-if "%progress%"=="1" goto suma
-if "%progress%"=="2" goto resta
-if "%progress%"=="3" goto multiplicacion
-if "%progress%"=="4" goto division
-
-:suma
-call progress/suma.bat
-
-:resta
-call progress/resta.bat
-
-:multiplicacion
-call progress/multiplicacion.bat
-
-:division
-call progress/division.bat
