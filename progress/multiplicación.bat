ec459dbb1e8734d1f85ce720cb9fdba16ba1c0b1
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-04-17 14:02:19 -0400

    Update multiplicación.bat

--- progress/multiplicación.bat
+++ progress/multiplicación.bat
@@ -8,9 +8,9 @@ echo.
 set /p nro2= Coloque la segunda cifra: 
 cls
 
-set /a resta= "%nro1%" x "%nro2%"
+set /a resta= "%nro1%" * "%nro2%"
 
 echo.
-echo El resultado de la suma es %multiplicacion%
+echo El resultado de la multiplicacion es %multiplicacion%
 pause >nul
 start inicio.bat
